ffac1dc4bc2c6794c2f7d1bd1ec72542e20cd06d
André Loreth <andre@loreth.co>
2023-11-03 09:29:07 +0100

    feat: enable new ESLint rules (#104)
    * feat: add additional rules from eslint-plugin-import
    
    * feat: enable "@typescript-eslint/return-await" rule
    
    * fix: use "./tsconfig.json" as default for the "project" parser option

--- packages/eslint-config-base/src/index.ts
+++ packages/eslint-config-base/src/index.ts
@@ -13,7 +13,18 @@ const config = [
 			import: eslintPluginImport,
 		},
 		rules: {
+			/**
+			 * Use the rules from the base config as defaults.
+			 *
+			 * @see https://alloyteam.github.io/eslint-config-alloy/?hideOff=1
+			 */
 			...(AlloyBase.rules ?? {}),
+
+			/**
+			 * Enforce a consistent order and grouping of import statements.
+			 *
+			 * @see https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md
+			 */
 			"import/order": [
 				"error",
 				{
@@ -30,6 +41,36 @@ const config = [
 					warnOnUnassignedImports: true,
 				},
 			],
+
+			/**
+			 * Enforce the "export" statement is placed at the end of the file.
+			 * This avoids sprinkling export statements throughout the file.
+			 *
+			 * @see https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/exports-last.md
+			 */
+			"import/exports-last": "warn",
+
+			/**
+			 * Import statements should always be the first statements in a file.
+			 * This makes it easier to identify the dependencies of a file.
+			 *
+			 * NOTE: Directives (e.g. "use strict") are allowed to come before import statements.
+			 *
+			 * @see https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/first.md
+			 */
+			"import/first": "error",
+
+			/**
+			 * Enforce a newline after the import statements.
+			 *
+			 * @see https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/newline-after-import.md
+			 */
+			"import/newline-after-import": "error",
+
+			/**
+			 * Disable the "no-return-await" rule.
+			 */
+			"no-return-await": "off",
 		},
 	},
 ] satisfies Linter.FlatConfig[];
--- packages/eslint-config-typescript/src/index.ts
+++ packages/eslint-config-typescript/src/index.ts
@@ -13,9 +13,29 @@ const config = [
 		},
 		languageOptions: {
 			parser: TypescriptEslintParser,
+			parserOptions: {
+				project: "./tsconfig.json",
+			},
 		},
 		rules: {
+			/**
+			 * Use the rules from the base config as defaults.
+			 *
+			 * @see https://alloyteam.github.io/eslint-config-alloy/?hideOff=1&rule=typescript
+			 */
 			...(AlloyTypescript.rules ?? {}),
+
+			/**
+			 * Enforce to use `return await` for async functions.
+			 * This rule is set to "always" because it's a good practice to use
+			 * `return await` for async functions.
+			 *
+			 * NOTE: This is overridden from the base config.
+			 *
+			 * @see https://typescript-eslint.io/rules/return-await/
+			 * @see https://stackoverflow.com/questions/43353087/are-there-performance-concerns-with-return-await/70979225#70979225
+			 */
+			"@typescript-eslint/return-await": ["warn", "always"],
 		},
 	},
 ] satisfies Linter.FlatConfig[];
