f4417aaecae9d5aca55b4f4799cd8a72a47a19be
Andre Loreth <andre@loreth.co>
2024-10-11 09:48:11 +0200

    fix: add eslint-plugin-react-hooks with recommended config

--- packages/eslint-config-react/shims.d.ts
+++ packages/eslint-config-react/shims.d.ts
@@ -2,6 +2,7 @@ declare module "eslint-config-alloy/react.js" {
 	import { Linter } from "eslint";
 
 	import BaseConfig = Linter.BaseConfig;
+
 	const config: Partial<BaseConfig<any>>;
 	export = config;
 }
@@ -10,6 +11,16 @@ declare module "eslint-plugin-react" {
 	import { Linter } from "eslint";
 
 	import Plugin = Linter.Plugin;
+
+	const plugin: Plugin;
+	export = plugin;
+}
+
+declare module "eslint-plugin-react-hooks" {
+	import { Linter } from "eslint";
+
+	import Plugin = Linter.Plugin;
+
 	const plugin: Plugin;
 	export = plugin;
 }
--- packages/eslint-config-react/src/index.ts
+++ packages/eslint-config-react/src/index.ts
@@ -1,5 +1,6 @@
 import AlloyReact from "eslint-config-alloy/react.js";
 import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 
 import type { Linter } from "eslint";
 
@@ -8,9 +9,19 @@ const config = [
 		files: ["**/*.{js,jsx,ts,tsx}"],
 		plugins: {
 			react: eslintPluginReact,
+			"react-hooks": eslintPluginReactHooks,
 		},
 		rules: {
 			...(AlloyReact.rules ?? {}),
+
+			/**
+			 * Apply the rules from the eslint-plugin-react-hooks package and use the recommended
+			 * configuration.
+			 *
+			 * @see https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks
+			 */
+			"react-hooks/rules-of-hooks": "error",
+			"react-hooks/exhaustive-deps": "warn",
 		},
 	},
 ] satisfies Linter.Config[];
