aae612013f475eac8833a08aff3614ba075c73f8
Andre Loreth <andre@loreth.co>
2026-03-08 10:53:42 +0100

    feat: add stylistic flavour

--- packages/eslint-config-base/src/configs/index.ts
+++ packages/eslint-config-base/src/configs/index.ts
@@ -1,4 +1,5 @@
 export { config as base } from "./base.js";
 export { config as configFiles } from "./flavour-config-files.js";
 export { config as nestjs } from "./flavour-nestjs.js";
+export { config as stylistic } from "./flavour-stylistic.js";
 export { config as vitest } from "./flavour-vitest.js";
