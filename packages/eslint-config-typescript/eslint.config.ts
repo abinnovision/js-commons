ed703773536955d64536b40be615b8f9fe36d3a4
André L. <andre@loreth.co>
2026-03-21 14:08:23 +0100

    build: remove legacy eslint-config-typescript package (#590)

--- packages/eslint-config-typescript/eslint.config.ts
+++
@@ -1,7 +0,0 @@
-import { base, configFiles, stylistic } from "@abinnovision/eslint-config-base";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-	{ extends: [base, stylistic] },
-	{ files: ["*.ts"], extends: [configFiles] },
-]);
--- packages/eslint-config-typescript/shims.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "eslint-config-alloy/typescript.js" {
-	import { Linter } from "eslint";
-
-	import BaseConfig = Linter.BaseConfig;
-	const config: Partial<BaseConfig<any>>;
-	export = config;
-}
