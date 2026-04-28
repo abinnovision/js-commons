4de4c562df5b43b15de7b52637c7d82bd15e498f
André L. <andre@loreth.co>
2026-04-28 11:00:41 +0200

    feat: support ESLint 10 in shared configs (#599)

--- packages/eslint-config-base/shims.d.ts
+++ packages/eslint-config-base/shims.d.ts
@@ -15,12 +15,3 @@ declare module "eslint-config-alloy/typescript.js" {
 	const config: Partial<BaseConfig<any>>;
 	export = config;
 }
-
-declare module "eslint-plugin-import" {
-	import { Linter } from "eslint";
-
-	import Plugin = Linter.Plugin;
-
-	const plugin: Plugin;
-	export = plugin;
-}
